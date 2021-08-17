7b15760182aa56d8d16181aeecce340c90a38a0a
mrclfd <mrclfd@yahoo.com>
2021-08-18 01:46:29 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Kloning Repo, Harap Tunggu..."
-git clone https://github.com/mrclfd/DEPLOY-RadioPlayerV3/ /DEPLOY-RadioPlayerV3
+git clone https://github.com/mrclfd/DEPLOY-RadioPlayerV3 /DEPLOY-RadioPlayerV3
 cd /DEPLOY-RadioPlayerV3
 echo "Mulai Bot, Harap Tunggu..."
 python3 main.py
